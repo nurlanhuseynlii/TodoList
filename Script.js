59f0149d237b19de976f0267f7cdb5a65aefdd46
nurlanhuseynli2002@gmail.com <nurlanhuseynli2002@gmail.com>
2023-01-23 02:59:57 +0400

    new file

--- Script.js
+++ Script.js
@@ -4,7 +4,11 @@ let btn1 = document.querySelector('#btn1');
 let btn2 = document.querySelector('#btn2');
 let btn3 = document.querySelector('#btn3');
 let list = document.querySelector('#list');
-let li = document.querySelector('#li');
+let contain = document.querySelector('.element-list');
+// let li = document.querySelectorAll("li");
+
+
+let li ;
 
 btn.addEventListener('click' , ()=>{
     let txt = input.value;
@@ -13,20 +17,78 @@ btn.addEventListener('click' , ()=>{
         
     }else{
         const randomColor = Math.floor(Math.random()*16777215).toString(16);
-        let li = document.createElement('li')
+        li = document.createElement('li')
+        li.classList.add("mystyle");
         li.innerHTML = txt;
         list.insertBefore(li, list.childNodes[0]);
         input.value = '';
         li.style.color = "#" + randomColor;
-        li.style.backgroundColor = "#" + randomColor;
+        list.style.backgroundColor = "#" + randomColor;
+        contain.style.backgroundColor = "#" + randomColor;
     }
 })
 
+
+// let lis = document.querySelectorAll('.mystyle');
+// for (let i = countStart; i < countEnd; i++) {
+//     console.log(lis[i]);
+//     // list += li;
+// }
+
 btn1.addEventListener('click',()=>{
-    for (let i = 0; i < 3; i++) {
-        console.log(li.value);
+    let countStart = 0;
+    let countEnd = 4;
+    const listItems = list.getElementsByTagName('li');
+    
+    for (let index = 0; index < listItems.length; index++) {
+        listItems[index].classList.add("hide");
+        console.log (listItems[index]);
+            // listItems[i].classList.add("show");
     }
+for (let i = countStart; i < countEnd; i++) {
+    console.log (listItems[i]);
+        listItems[i].classList.add("show");
+        listItems[i].classList.remove("hide");
+}
+    // list.innerHTML = li;
 })
+
+btn2.addEventListener('click',()=>{
+    let countStart = 4;
+    let countEnd = 8;
+    const listItems = list.getElementsByTagName('li');
+    
+    for (let index = 0; index < listItems.length; index++) {
+        listItems[index].classList.add("hide");
+        console.log (listItems[index]);
+            // listItems[i].classList.add("show");
+    }
+for (let i = countStart; i < countEnd; i++) {
+    console.log (listItems[i]);
+        listItems[i].classList.add("show");
+        listItems[i].classList.remove("hide");
+}
+    // list.innerHTML = li;
+})
+btn3.addEventListener('click',()=>{
+    let countStart = 8;
+    let countEnd = 12;
+    const listItems = list.getElementsByTagName('li');
+    
+    for (let index = 0; index < listItems.length; index++) {
+        listItems[index].classList.add("hide");
+        console.log (listItems[index]);
+            // listItems[i].classList.add("show");
+    }
+for (let i = countStart; i < countEnd; i++) {
+    console.log (listItems[i]);
+        listItems[i].classList.add("show");
+        listItems[i].classList.remove("hide");
+}
+    // list.innerHTML = li;
+})
+
+
 
 
 
