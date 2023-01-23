b49238a0cad44786db066cf0d352ffa75f9403f1
nurlanhuseynli2002@gmail.com <nurlanhuseynli2002@gmail.com>
2023-01-23 16:55:32 +0400

    TodoApp

--- Script.js
+++ Script.js
@@ -1,19 +1,16 @@
 let input = document.querySelector('#add');
 let btn = document.querySelector('#btn');
-let btn1 = document.querySelector('#btn1');
-let btn2 = document.querySelector('#btn2');
-let btn3 = document.querySelector('#btn3');
 let list = document.querySelector('#list');
 let contain = document.querySelector('.element-list');
-// let li = document.querySelectorAll("li");
+let paginate = document.querySelector('.pagination');
 
 
 let li ;
 
 btn.addEventListener('click' , ()=>{
-    let txt = input.value;
+    let txt = input.value.trim();
     if(txt === ""){
-        alert('doldurun')
+        alert('My str length');
         
     }else{
         const randomColor = Math.floor(Math.random()*16777215).toString(16);
@@ -21,72 +18,32 @@ btn.addEventListener('click' , ()=>{
         li.classList.add("mystyle");
         li.innerHTML = txt;
         list.insertBefore(li, list.childNodes[0]);
+        pagination();
         input.value = '';
         li.style.color = "#" + randomColor;
-        list.style.backgroundColor = "#" + randomColor;
         contain.style.backgroundColor = "#" + randomColor;
     }
 })
 
 
-// let lis = document.querySelectorAll('.mystyle');
-// for (let i = countStart; i < countEnd; i++) {
-//     console.log(lis[i]);
-//     // list += li;
-// }
 
-btn1.addEventListener('click',()=>{
-    let countStart = 0;
-    let countEnd = 4;
-    const listItems = list.getElementsByTagName('li');
-    
-    for (let index = 0; index < listItems.length; index++) {
-        listItems[index].classList.add("hide");
-        console.log (listItems[index]);
-            // listItems[i].classList.add("show");
-    }
-for (let i = countStart; i < countEnd; i++) {
-    console.log (listItems[i]);
-        listItems[i].classList.add("show");
-        listItems[i].classList.remove("hide");
+
+const pagination = () => {
+    paginate.innerHTML = "";
+let lis = document.querySelectorAll('.mystyle');
+  for (let i = 0; i < Math.ceil(lis.length / 5); i++) {
+    paginate.innerHTML += `
+        <button onclick="Page(${i})">${i + 1}</button>
+        `;     
+  }
 }
-    // list.innerHTML = li;
-})
 
-btn2.addEventListener('click',()=>{
-    let countStart = 4;
-    let countEnd = 8;
-    const listItems = list.getElementsByTagName('li');
-    
-    for (let index = 0; index < listItems.length; index++) {
-        listItems[index].classList.add("hide");
-        console.log (listItems[index]);
-            // listItems[i].classList.add("show");
-    }
-for (let i = countStart; i < countEnd; i++) {
-    console.log (listItems[i]);
-        listItems[i].classList.add("show");
-        listItems[i].classList.remove("hide");
+const Page = (a) => {
+    document.querySelectorAll("li").forEach((a) => a.classList.add("hide"));
+  for (let i = a * 5; i < a * 5 + 5; i++) {
+    document.querySelectorAll("li")[i].classList.remove("hide");
+  }
 }
-    // list.innerHTML = li;
-})
-btn3.addEventListener('click',()=>{
-    let countStart = 8;
-    let countEnd = 12;
-    const listItems = list.getElementsByTagName('li');
-    
-    for (let index = 0; index < listItems.length; index++) {
-        listItems[index].classList.add("hide");
-        console.log (listItems[index]);
-            // listItems[i].classList.add("show");
-    }
-for (let i = countStart; i < countEnd; i++) {
-    console.log (listItems[i]);
-        listItems[i].classList.add("show");
-        listItems[i].classList.remove("hide");
-}
-    // list.innerHTML = li;
-})
 
 
 
@@ -95,12 +52,3 @@ for (let i = countStart; i < countEnd; i++) {
 
 
 
-
-// document.getElementById("list").pagination({
-//     dataSource: [1, 2, 3, 4],
-//     callback: function(data, pagination) {
-        
-//         let html = template(data);
-//         dataContainer.innerHTML = html;
-//     }
-// })
